ccc769cabea5e60667ad41ec8d940d28e25e685d
Teikane <kiet.nguyenktn@gmail.com>
2017-06-09 09:29:10 -0700

    Update views.py
    Added form functionality to views.

--- anisongDB/app/views.py
+++ anisongDB/app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +8,8 @@ def index():
 	# Create fake user and render home page template
 	user ={'nickname': 'admin'}
 	return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title = 'Log In', form=form)
