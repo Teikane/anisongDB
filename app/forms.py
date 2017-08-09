431b8077d9f5e7ea6509dd82249a8a64b28eab4a
Teikane <kiet.nguyenktn@gmail.com>
2017-08-09 08:39:25 -0700

    Added Signup Form

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,9 @@ class LoginForm(Form):
 	username = StringField('username', validators=[DataRequired()])
 	password = StringField('password', validators=[DataRequired()])
 	remember_me = BooleanField('remember_me', default=False)
+	
+class SignupForm(Form):
+	username = StringField('username', validators=[DataRequired()])
+	password = PasswordField('password', validators=[DataRequired()])
+	confirm_password = PasswordField('password', validators=[DataRequired(), EqualTo('password')])
+	email = StringField('password', validators=[DataRequired(), Email()])
