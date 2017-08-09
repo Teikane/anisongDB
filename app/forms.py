e35f0c605cc1fe0fde6e5f50eb5dd4133d803ddb
Teikane <kiet.nguyenktn@gmail.com>
2017-08-09 08:39:49 -0700

    Updated imports

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(Form):
 	username = StringField('username', validators=[DataRequired()])
