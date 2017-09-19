1a591cd7799d5d6330842a49ca76c636d1347ee2
Teikane <kiet.nguyenktn@gmail.com>
2017-09-18 21:22:46 -0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-#enable basic webform XXS prevention
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'insert-difficult-to-guess-phrase-here-jinrui-ni-eiko-aru'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'anisong.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
